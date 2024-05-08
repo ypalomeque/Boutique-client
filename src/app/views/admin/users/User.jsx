212f25b6bec8e50453e6d7ed3bc44bdda6bb34cc
Yirleison Palomeque <ppalomeque1988@gmail.com>
2024-05-07 23:54:19 -0500

    Se crea componente tabla

--- src/app/views/admin/users/User.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { memo } from "react";
-
-const User = () => {
-  return <div>Vista Clientes Para programar</div>;
-};
-
-export default memo(User);
--- src/app/views/admin/users/UserRoute.jsx
+++
@@ -1,6 +0,0 @@
-import Loadable from "app/components/Loadable";
-import { lazy } from "react";
-
-const USER = Loadable(lazy(() => import("./User")));
-
-export const userRoute = { path: "/usuarios", element: <USER /> };
